49b74c244139237d882ce5138cbe18107e020635
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-06 22:31:23 -0600

    VB radiom de un circulo y calcular su area y longitud

---
+++ Area de un circulo/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button_Run = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox_Radio = New System.Windows.Forms.TextBox()
+        Me.Label_Area = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label_Longitud = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button_Run
+        '
+        Me.Button_Run.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_Run.Location = New System.Drawing.Point(97, 288)
+        Me.Button_Run.Name = "Button_Run"
+        Me.Button_Run.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Run.TabIndex = 0
+        Me.Button_Run.Text = "Run"
+        Me.Button_Run.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(77, 158)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ingrese el radio"
+        '
+        'TextBox_Radio
+        '
+        Me.TextBox_Radio.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_Radio.Location = New System.Drawing.Point(80, 187)
+        Me.TextBox_Radio.Name = "TextBox_Radio"
+        Me.TextBox_Radio.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox_Radio.TabIndex = 3
+        '
+        'Label_Area
+        '
+        Me.Label_Area.AutoSize = True
+        Me.Label_Area.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Area.Location = New System.Drawing.Point(89, 222)
+        Me.Label_Area.Name = "Label_Area"
+        Me.Label_Area.Size = New System.Drawing.Size(16, 16)
+        Me.Label_Area.TabIndex = 4
+        Me.Label_Area.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(34, 222)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Area :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 252)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Longitud:"
+        '
+        'Label_Longitud
+        '
+        Me.Label_Longitud.AutoSize = True
+        Me.Label_Longitud.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.150944!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Longitud.Location = New System.Drawing.Point(89, 252)
+        Me.Label_Longitud.Name = "Label_Longitud"
+        Me.Label_Longitud.Size = New System.Drawing.Size(16, 16)
+        Me.Label_Longitud.TabIndex = 7
+        Me.Label_Longitud.Text = "0"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(71, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(129, 136)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(263, 323)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label_Longitud)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label_Area)
+        Me.Controls.Add(Me.TextBox_Radio)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button_Run)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button_Run As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_Radio As System.Windows.Forms.TextBox
+    Friend WithEvents Label_Area As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label_Longitud As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Area de un circulo/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Circulo()
+        'Declaramos las variables
+        Dim area, longitud, radio As Double
+        'Obtenemos el datos ingresado
+        radio = Convert.ToDouble(TextBox_Radio.Text)
+        'Formula para calcular el área del circulo
+        area = Math.PI * (radio ^ 2)
+        'Formula para calcular la longitud del circulo
+        longitud = 2 * Math.PI * radio
+        'Mostramos los resultados en las label
+        Label_Area.Text = area.ToString() & " cm"
+        Label_Longitud.Text = longitud.ToString() & " cm"
+    End Sub
+
+    Private Sub Button_Run_Click(sender As Object, e As EventArgs) Handles Button_Run.Click
+        Circulo()
+    End Sub
+End Class
